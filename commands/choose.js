c9ad6ee1f589d7328016143ade6c46644e03f57b
Federico Pigato <federico.pigato2001@gmail.com>
2020-12-05 16:34:44 +0100

    added choose

--- commands/choose.js
+++ commands/choose.js
@@ -3,22 +3,20 @@ module.exports = {
     description: "option1 option2 ...",
     whatDo: "Choose between options",
     execute(message, args) {
-    let index = Math.floor(Math.random() * args.length);
+        let index = Math.floor(Math.random() * args.length);
 
-    message.channel.send({embed: {
-        color: "#f0cf29",
-        fields: [{
-            name: "I choose",
-            value: args[index]
-          }
-        ],
-        footer: {
-            text: "by Pyguz.#0456",
-            icon_url: "https://cdn.discordapp.com/avatars/484387014725206016/4113368f74bd7056a02b20b03b2995a3.png"
-          }
-      }
-    });
-      
-    message.channel.send(embed);
+        message.channel.send({embed: {
+            color: "#f0cf29",
+            fields: [{
+                name: "I choose",
+                value: args[index]
+            }
+            ],
+            footer: {
+                text: "by Pyguz.#0456",
+                icon_url: "https://cdn.discordapp.com/avatars/484387014725206016/4113368f74bd7056a02b20b03b2995a3.png"
+            }
+        }
+        });
     }
 }
