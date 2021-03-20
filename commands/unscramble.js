22e04c065ea59e8b3a38ce086b452ff64096e493
Federico Pigato <federico.pigato2001@gmail.com>
2021-03-20 20:59:49 +0100

    Unscramble command with embed message

--- commands/unscramble.js
+++ commands/unscramble.js
@@ -6,6 +6,19 @@ module.exports = {
     usage: "<word>",
     modOnly: false,
     execute(message, args) {
-        message.channel.send(unscramble(args[0]));
+        message.channel.send({embed: {
+            color: "#ffb400",
+            description: "Unscrambled " + args[0],
+            fields: [{
+                name: unscramble(args[0]),
+                value: "\u200b"
+            }
+            ],
+            footer: {
+                text: "by Pyguz.#0456",
+                icon_url: "https://cdn.discordapp.com/avatars/484387014725206016/4113368f74bd7056a02b20b03b2995a3.png"
+            }
+        }
+        });
     }
 }
