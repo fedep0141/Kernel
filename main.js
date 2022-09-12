57d7b55c10be55c326352068d5a1e13adc4d7547
Federico Pigato <federico.pigato2001@gmail.com>
2022-09-12 22:05:07 +0200

    added invite command and scheduled message

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { Collection, GatewayIntentBits } = require('discord.js');
+const { Collection, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const roleClaim = require("./reactionRoles/reactionRoles.js");
 const FS = require("fs");
 require('dotenv').config();
@@ -15,7 +15,8 @@ const client = new Discord.Client({
         GatewayIntentBits.GuildPresences,
         GatewayIntentBits.GuildEmojisAndStickers,
         GatewayIntentBits.GuildBans,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
     ],
     partials: [
         "USER",
@@ -24,6 +25,21 @@ const client = new Discord.Client({
         "REACTION"
     ]
 })
+
+const schedule = require('node-schedule');
+
+const rule = new schedule.RecurrenceRule();
+rule.hour = 8;
+
+schedule.scheduleJob(rule, function () {
+
+    let embed = new EmbedBuilder()
+        .setColor("#ffb400")
+        .setTitle("BUONGIORNO")
+        .setFooter({ text: "by Pyguz.#0456", iconURL: "https://cdn.discordapp.com/avatars/484387014725206016/4113368f74bd7056a02b20b03b2995a3.png" })
+
+    client.channels.cache.get("1016099553843347607").send({ embeds: [embed] })
+});
 
 client.commands = new Collection();
 client.commandsArray = [];
