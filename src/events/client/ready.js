8288ba017fa298d63a6dec2e23a427a752c53f02
Federico Pigato <federico.pigato2001@gmail.com>
2025-01-25 21:53:50 +0100

    commented the welcome message

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
-        await scheduledMessages.execute(client)
+        // await scheduledMessages.execute(client)
         await roleClaim(client);
         console.log(`${client.user.tag} is online.`)
     }
